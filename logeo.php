916bcdae355ea6770e562d0fa9809114f1451b0e
JuanK2019 <juancavios16@hotmail.com>
2019-04-18 12:07:24 -0500

    Logueo actividad 3
    Queda pendiente programar el menú Mi cuenta

--- logeo.php
+++ logeo.php
@@ -1,6 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Logeo</title>
+
+  <!--JQUERY -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  
+  <!--SOPORTE DE BOTAS DE MARCO para el estilo de la pagina-->
+  <link rel="stylesheet"  href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+  
+  <!--Los iconos tipo Solid de Fontawesome-->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/solid.css">
+  <script src="https://use.fontawesome.com/releases/v5.0.7/js/all.js"></script>
+
+  <!--Nuestro css-->
+  <link rel="stylesheet" type="text/css" href="css/index.css" th:href="@{/css/index.css}">
+</head>
+
 <?php 
 include("includes/layout/header.php");
 include("conn/clases.php");
+
+if (isset($_POST["Salir"])) {
+  $email = $_POST["emailLog"];
+  $sql = "UPDATE usuarios SET acceso = '0' WHERE email = '$email'";
+  $db = new db();
+  $result = $db->db_sql($sql);
+}
 
 if(isset($_POST["Ingresar"])){
   $email = $_POST["email"];
@@ -12,58 +40,87 @@ if(isset($_POST["Ingresar"])){
 
   if(is_object($result)){
     $row = $result->fetch_assoc();
-    if(password_verify($password, $row['password'])){ ?>
-      <div class="alert alert-success">
+    if(password_verify($password, $row['password']) && $email == $row['email']){
+      /*<div class="alert alert-success container">
         <button type="button" class="close" data-dismiss="alert">&times;</button>
-        <strong>Bienvenido!</strong> <?php echo "<br>". $row['nombre1'] . " " . $row['apellido1'] ?>
-      </div>
-    <?php }else{ ?>
-      <div class="alert alert-warning alert-dismissible fade show">
-        <button type="button" class="close" data-dismiss="alert">&times;</button>
-        <p>El usuario o contraseña son inválidos.<br> Por favor inténtelo nuevamente o<a href="registro.php"> regístrese como nuevo usuario</a></p>        
+        <strong>Bienvenido!</strong> <?php echo "<br> ". $row['nombre1'] . " " . $row['apellido1'] ?>
+      </div>*/
+      $sql = "UPDATE usuarios SET acceso = '1', fecha_acceso = CURDATE() WHERE email = '$email'";
+      $result = $db->db_sql($sql);?>
+      <header>
+        <ul class="nav nav-tabs" id="cuenta">
+          <li>
+            <label type="text" for="email" class="label" id="label"><?php echo $email ?></label>
+          </li>         
+          <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Mi cuenta</a>
+              <div class="dropdown-menu">
+              <a class="dropdown-item" href="#">Mi perfil</a>
+              <a class="dropdown-item" href="#">Mis contactos</a>
+              </div>
+          </li>
+          <li>
+            <form method="POST">
+              <input type="email" name="emailLog" id="emailLog" class="form-control" value="<?php echo $email ?>">
+              <button type="submit" class="btn btn-primary" name="Salir" id="Salir" style="height:42px; width:100px">Salir  <i class="fas fa-sign-out-alt"></i></button>
+            </form>     
+          </li>
+        </ul> 
+      </header>
+      <?php if (is_object($result)) {
+          $row = $result->fetch_assoc();
+          echo "Logueo actualizado ".$row['acceso']. " y ".$row['fecha_acceso'];
+      }
+    }else{ ?>
+      <div class="alert alert-warning container" id="mensaje">
+        <!--<button type="button" class="close" data-dismiss="alert">&times;</button>-->
+        <p>El usuario o contraseña son inválidos.<br> Por favor <a href="logeo.php">inténtelo de nuevo</a> o<a href="registro.php"> regístrese como nuevo usuario</a></p>
       </div>
     <?php }
   }
-}
-?>
-
-<!DOCTYPE html>
-<html lang="en">
+}else{ ?>
 <head>
   <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Logeo</title>
 
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  <!--JQUERY -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  
+  <!--SOPORTE DE BOTAS DE MARCO para el estilo de la pagina-->
+  <link rel="stylesheet"  href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+  
+  <!--Los iconos tipo Solid de Fontawesome-->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/solid.css">
+  <script src="https://use.fontawesome.com/releases/v5.0.7/js/all.js"></script>
 
-
-  <title>Iniciar Sesión</title>
+  <!--Nuestro css-->
+  <link rel="stylesheet" type="text/css" href="css/index.css" th:href="@{/css/index.css}">
 </head>
 <body>
-    <div class="container">
-      <br>
-      <h2>Ingresar</h2>
-      <div class="row">
-        <div class="col-4">
-          <br>
-          <br>
-          <form action="logeo.php" method="post">
-            <div class="form-group">
-              <label for="email">Dirección de Correo</label>
-              <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Ingresa el Correo" name="email" required="true" autofocus="true">
-              <small id="emailHelp" class="form-text text-muted">La dirección de correo es secreta</small>
-            </div>
-            <div class="form-group">
-              <label for="password">Contraseña</label>
-              <input type="password" class="form-control" id="password" placeholder="contraseña" name="password" required="true">
-            </div>
-            <button type="submit" class="btn btn-primary" name="Ingresar">Ingresar</button>
-          </form>
+  <div class="modal-dialog text-center">
+    <div class="col-sm-8 main-section">
+      <div class="modal-content">
+        <div class="col-12 user-img">
+          <img src="img/login.png"/>
+        </div>
+        <form class="col-12" method="POST">
+          <div class="form-group" id="user-group">
+            <input type="email" class="form-control" placeholder="Nombre de usuario" name="email" id="email">
+          </div>
+          <div class="form-group" id="contrasena-group">
+            <input type="password" class="form-control" placeholder="Contraseña" name="password" id="password">
+          </div>
+          <button type="submit" class="btn btn-primary" name="Ingresar" id="Ingresar"><i class="fas fa-sign-in-alt"></i>  Ingresar</button>
+        </form>
+        <div class="col-12 forgot">
+          <a href="rememberPass.php">Recordar contraseña</a>
         </div>
       </div>
     </div>
+  </div>
 </body>
 </html>
+<?php
+}
+?>
